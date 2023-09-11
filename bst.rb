352e2e99c672f44ff7bcda7735bceadafc3f353b
Funto <funto.afuwape@gmail.com>
2023-09-11 12:17:00 +0100

    Create Tree#build_tree
    This recursive method takes an array, a start index, and an end index and uses this to build a balanced binary search tree.
    
    The Tree#pretty_print method was gotten from The Odin Project and helps to visualise the BST.

--- bst.rb
+++ bst.rb
@@ -1,4 +1,5 @@
 class Node
+  attr_accessor :left_node, :right_node
   include Comparable
   attr :data
   def <=>(other_node)
@@ -14,6 +15,22 @@ end
 
 class Tree
   def initialize(arr)
-    @arr = arr.to_a.uniq
+    @arr = arr.to_a.sort.uniq
+    @root = build_tree(@arr, 0, @arr.size - 1)
+  end
+
+  def build_tree(arr, arr_start, arr_end)
+    return nil if arr_start > arr_end
+    middle = (arr_start + arr_end) / 2
+    node = Node.new(arr[middle])
+    node.left_node = build_tree(arr, arr_start, middle - 1)
+    node.right_node = build_tree(arr, middle + 1, arr_end)
+    return node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_node, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_node
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left_node, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_node
   end
 end
